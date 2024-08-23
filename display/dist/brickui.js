aa1d06a47c30114797a9f0e1c0ef1fb79437f11d
EgorYakunin <e.yakunin04@gmail.com>
2024-08-23 17:00:09 +0300

    card: adjusted padding value

--- display/dist/brickui.js
+++ display/dist/brickui.js
@@ -1,12 +1,12 @@
-import { jsx as t } from "react/jsx-runtime";
-import './index.css';const s = "_base_1c565_1", l = "_default_spacing_1c565_7", i = "_outline_1c565_12", a = {
+import { jsx as c } from "react/jsx-runtime";
+import './index.css';const s = "_base_3jjh0_1", l = "_default_spacing_3jjh0_7", i = "_outline_3jjh0_18", t = {
   base: s,
   default_spacing: l,
   outline: i
 };
 function y(e) {
-  let n = a.base;
-  return e.no_spacing || (n += " " + a.default_spacing), e.outline && (n += " " + a.outline), e.className && (n += " " + e.className), /* @__PURE__ */ t(
+  let n = t.base;
+  return e.no_spacing || (n += " " + t.default_spacing), e.outline && (n += " " + t.outline), e.className && (n += " " + e.className), /* @__PURE__ */ c(
     "div",
     {
       className: n,
@@ -15,15 +15,15 @@ function y(e) {
     }
   );
 }
-const _ = "_base_174nd_1", d = "_accent_174nd_8", o = "_secondary_174nd_13", u = "_outline_174nd_18", c = {
+const _ = "_base_174nd_1", d = "_accent_174nd_8", o = "_secondary_174nd_13", u = "_outline_174nd_18", a = {
   base: _,
   accent: d,
   secondary: o,
   outline: u
 };
 function b(e) {
-  let n = c.base;
-  return e.accent && (n += " " + c.accent), e.outline && (n += " " + c.outline), e.secondary && (n += " " + c.secondary), e.className && (n += " " + e.className), /* @__PURE__ */ t(
+  let n = a.base;
+  return e.accent && (n += " " + a.accent), e.outline && (n += " " + a.outline), e.secondary && (n += " " + a.secondary), e.className && (n += " " + e.className), /* @__PURE__ */ c(
     "div",
     {
       className: n,
