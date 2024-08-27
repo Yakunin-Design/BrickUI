dfbb61df13f60696437fd4b52d187b3a1e308d46
EgorYakunin <e.yakunin04@gmail.com>
2024-08-28 00:40:05 +0300

    lib: fixed bug with toggle in useBoolean

--- lib/src/hooks/useBoolean.ts
+++ lib/src/hooks/useBoolean.ts
@@ -4,8 +4,10 @@ export default function useBoolean(initial_state?: boolean): [boolean, (value?: 
 	const [toggle_value, set_toggle_value] = useState(initial_state ? initial_state : false);
 
 	function toggler(value?: boolean) {
-		if(value) set_toggle_value(value);
-		else set_toggle_value(prev => !prev);
+		if(value === true) set_toggle_value(true)
+			else if(value === false) set_toggle_value(false)
+				else set_toggle_value(prev => !prev);
 	}
+
 	return [toggle_value, toggler];
 };
